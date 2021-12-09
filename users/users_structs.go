dd50d3047140ac19add0d707db95d6ec6459cda2
batuhannoz <b.ozdmr23@gmail.com>
2021-12-09 12:45:13 +0300

    Added get_users_top_artists.go

--- users/users_structs.go
+++ users/users_structs.go
@@ -26,3 +26,33 @@ type CurrentUsersProfileResponse struct {
 	Type    string `json:"type"`
 	Uri     string `json:"uri"`
 }
+
+type GetUsersTopArtistsResponse struct {
+	Items []struct {
+		ExternalUrls struct {
+			Spotify string `json:"spotify"`
+		} `json:"external_urls"`
+		Followers struct {
+			Href  interface{} `json:"href"`
+			Total int         `json:"total"`
+		} `json:"followers"`
+		Genres []string `json:"genres"`
+		Href   string   `json:"href"`
+		Id     string   `json:"id"`
+		Images []struct {
+			Height int    `json:"height"`
+			Url    string `json:"url"`
+			Width  int    `json:"width"`
+		} `json:"images"`
+		Name       string `json:"name"`
+		Popularity int    `json:"popularity"`
+		Type       string `json:"type"`
+		Uri        string `json:"uri"`
+	} `json:"items"`
+	Total    int    `json:"total"`
+	Limit    int    `json:"limit"`
+	Offset   int    `json:"offset"`
+	Previous string `json:"previous"`
+	Href     string `json:"href"`
+	Next     string `json:"next"`
+}
--- users/users_urls.go
+++ users/users_urls.go
@@ -2,4 +2,5 @@ package spotify
 
 const (
 	GetCurrentUsersProfileURL = "https://api.spotify.com/v1/me"
+	GetUsersTopArtistsURL     = "https://api.spotify.com/v1/me/top/artists"
 )
