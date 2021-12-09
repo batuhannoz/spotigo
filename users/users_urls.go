d81f319dcdb5db9b3a5a2c41f42294e54d74c0b9
batuhannoz <b.ozdmr23@gmail.com>
2021-12-09 20:36:03 +0300

    Added follow_artist_or_user.go

--- users/users_urls.go
+++ users/users_urls.go
@@ -8,4 +8,5 @@ const (
 	FollowPlaylistURL         = "https://api.spotify.com/v1/playlists/"
 	UnfollowPlaylistURL       = "https://api.spotify.com/v1/playlists/"
 	GetFollowedArtistsURL     = "https://api.spotify.com/v1/me/following"
+	FollowArtistOrUserURL     = "https://api.spotify.com/v1/me/following"
 )
