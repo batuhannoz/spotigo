f2abdbb628e3a6fe601e7c83cdb9e270afcb6f31
batuhannoz <b.ozdmr23@gmail.com>
2021-12-12 16:48:04 +0300

    Added saved_show.go

---
+++ TODO.go
@@ -0,0 +1,11 @@
+package spotigo
+
+// Future innovations.
+// TODO Get Several Albums. - albums
+// TODO Search. - albums
+// TODO Save Albums. - albums
+// TODO Remove Albums. - albums
+// TODO Check Saved Albums. - albums
+// TODO Get Several Artists. -  artists
+// TODO Get Artists Albums. - artists
+//
