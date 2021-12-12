c7cf0020cb9035017067231a3257dae276ec71fa
batuhannoz <b.ozdmr23@gmail.com>
2021-12-12 16:58:32 +0300

    Added saved_episode.go

--- TODO.go
+++ TODO.go
@@ -8,4 +8,8 @@ package spotigo
 // TODO Check Saved Albums. - albums
 // TODO Get Several Artists. -  artists
 // TODO Get Artists Albums. - artists
+// TODO Get User's Saved Shows. - shows
+// TODO Remove User's Saved Shows. - shows
+// TODO Check User's Saved Shows. - shows
+// TODO Get Several Episodes. - episodes
 //
